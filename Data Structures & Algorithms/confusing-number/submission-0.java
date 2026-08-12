5109f92fe0f6bbc0d389abdf144c962bf6ff2e6f
0Raj2 <rajasekark0@gmail.com>
2026-08-12 22:45:36 +0530

    Add: confusing-number - submission-0

---
+++ Data Structures & Algorithms/confusing-number/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean confusingNumber(int n) {
+        
+        Map<Character, Character> invertMap = new HashMap<>() {{
+            put('0', '0');
+            put('1', '1');
+            put('6', '9');
+            put('8', '8');
+            put('9', '6');
+        }};
+        StringBuilder sb = new StringBuilder();
+
+        
+        for (char ch : String.valueOf(n).toCharArray()) {
+            if (!invertMap.containsKey(ch)) {
+                return false;
+            }
+            sb.append(invertMap.get(ch));
+        }
+
+        sb.reverse();
+        return Integer.parseInt(sb.toString()) != n;
+    }
+}
