5934e9bf1369c90ae6fb86d9f4bf195365145519
0Raj2 <rajasekark0@gmail.com>
2026-09-03 22:43:40 +0530

    Add: palindrome-linked-list - submission-1

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-1.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        ListNode copyHead = copyList(head);
+
+        ListNode reversedCopy = reverseList(copyHead);
+
+        ListNode p1 = head;
+        ListNode p2 = reversedCopy;
+        while (p1 != null && p2 != null) {
+            if (p1.val != p2.val) {
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return true;
+    }
+
+    private ListNode copyList(ListNode head) {
+        if (head == null) return null;
+
+        ListNode newHead = new ListNode(head.val);
+        ListNode currentOriginal = head.next;
+        ListNode currentCopy = newHead;
+
+        while (currentOriginal != null) {
+            currentCopy.next = new ListNode(currentOriginal.val);
+            currentCopy = currentCopy.next;
+            currentOriginal = currentOriginal.next;
+        }
+
+        return newHead;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
